f8511d2a14137d252ec96fbe1a1e8c186252b952
Merilynn Paisley <31383109+furudean@users.noreply.github.com>
2026-09-02 14:33:38 -0700

    support arm in cask
    this is broken until the next release (should be shortly)

--- Casks/lltexturecache-browser-qt.rb
+++ Casks/lltexturecache-browser-qt.rb
@@ -1,15 +1,17 @@
 cask "lltexturecache-browser-qt" do
+  arch arm: "arm64", intel: "x86_64"
+
   version "0.5.1"
 
-  sha256 "9dd2f321e12ff436090c0019ef38738a63e96847c4d01fd04d6b371eb9381617"
+  sha256 arm:   "9dd2f321e12ff436090c0019ef38738a63e96847c4d01fd04d6b371eb9381617",
+         intel: "9dd2f321e12ff436090c0019ef38738a63e96847c4d01fd04d6b371eb9381617"
 
-  url "https://github.com/furudean/lltexturecache-viewer-gui/releases/download/v#{version}/lltexturecache-browser-qt-#{version}-macos-arm64.zip"
+  url "https://github.com/furudean/lltexturecache-viewer-gui/releases/download/v#{version}/lltexturecache-browser-qt-#{version}-macos-#{arch}.zip"
   name "lltexturecache-browser-qt"
   desc "Browse and export textures from the Second Life texture cache"
   homepage "https://github.com/furudean/lltexturecache-viewer-gui"
 
-  depends_on arch: :arm64
-  depends_on macos: :sequoia
+  depends_on macos: :ventura
 
   app "lltexturecache-browser-qt.app"
 
