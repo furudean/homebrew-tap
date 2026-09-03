8e9eeb96a1619cafc440ac5fc10ba628dd330172
Merilynn Paisley <31383109+furudean@users.noreply.github.com>
2026-09-03 13:00:54 -0700

    setup for universal release

--- Casks/lltexturecache-browser-qt.rb
+++ Casks/lltexturecache-browser-qt.rb
@@ -1,12 +1,9 @@
 cask "lltexturecache-browser-qt" do
-  arch arm: "arm64", intel: "x86_64"
-
   version "0.5.4"
 
-  sha256 arm:   "06c5d1919030c48ca627522192c89f83d8ddfb849d23e1b130e0fdbd620eb8b4",
-         intel: "a24d48227c0d09fa621d0a004715e6c2b9ca6c5c7d1cd7f10c9dbbd2b617f721"
+  sha256 "06c5d1919030c48ca627522192c89f83d8ddfb849d23e1b130e0fdbd620eb8b4"
 
-  url "https://github.com/furudean/lltexturecache-viewer-gui/releases/download/v#{version}/lltexturecache-browser-qt-#{version}-macos-#{arch}.zip"
+  url "https://github.com/furudean/lltexturecache-viewer-gui/releases/download/v#{version}/lltexturecache-browser-qt-#{version}-macos-universal.zip"
   name "lltexturecache-browser-qt"
   desc "Browse and export textures from the Second Life texture cache"
   homepage "https://github.com/furudean/lltexturecache-viewer-gui"
